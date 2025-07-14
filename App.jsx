b82bf5b0a293edd47f0b97f7ca7a2facd7b04291
nagbhushank <k.abhikumbar.ak@gmail.com>
2025-07-14 21:05:23 +0530

    This is my first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+const App = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <h1>Products</h1>
+    </div>
+  );
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<App />);
